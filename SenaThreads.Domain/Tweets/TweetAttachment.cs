b1139670d7bf85c23808d5554c1d373cf6841041
Juan Bravo <juandiego732000@gmail.com>
2024-05-23 15:39:10 -0500

    Change TweetAttachment property name

--- SenaThreads.Domain/Tweets/TweetAttachment.cs
+++ SenaThreads.Domain/Tweets/TweetAttachment.cs
@@ -7,12 +7,12 @@ public class TweetAttachment : Entity
     public Guid Id { get; private set; }
     public Tweet Tweet { get; private set; }
     public Guid TweetId { get; private set; }
-    public string BlobOrS3Key { get; private set; }
+    public string Key { get; private set; }
 
-    public TweetAttachment(Guid tweetId, string blobOrS3Key)
+    public TweetAttachment(Guid tweetId, string key)
     {
         Id = Guid.NewGuid();
         TweetId = tweetId;
-        BlobOrS3Key = blobOrS3Key;
+        Key = key;
     }
 }
