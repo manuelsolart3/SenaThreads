3689bd5fe12d6fae06743cc4a9d4e692dc808519
Juan Bravo <juandiego732000@gmail.com>
2024-06-07 15:33:45 -0500

    Add Pageable

---
+++ SenaThreads.Domain/Abstractions/Pageable.cs
@@ -0,0 +1,8 @@
+﻿namespace SenaThreads.Domain.Abstractions;
+
+public class Pageable<TEntity>
+    where TEntity : class
+{
+    public List<TEntity> List { get; set; }
+    public int Count { get; set; }
+}
