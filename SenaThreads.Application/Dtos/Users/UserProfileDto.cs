8822b5a922b40e5f842dcd54903e8bdcc111e91f
manuelsolart3 <118754640+manuelsolart3@users.noreply.github.com>
2024-07-09 10:09:28 -0500

    [Add] PhoneNumber & DateOfBirth in GetUserProfile

--- SenaThreads.Application/Dtos/Users/UserProfileDto.cs
+++ SenaThreads.Application/Dtos/Users/UserProfileDto.cs
@@ -6,6 +6,8 @@ public class UserProfileDto
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string City { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public  string PhoneNumber { get; set; }
     public string Biography { get; set; }
     public string ProfilePictureS3Key { get; set; }
 }
