e37aadbf3e309e92ccf46b7f4569f0d18165af3c
manuelsolart3 <118754640+manuelsolart3@users.noreply.github.com>
2024-07-09 20:51:53 -0500

    [Add] UserData in GetUserSearchHistory

--- SenaThreads.Application/Dtos/Users/UserSearchHistoryDto.cs
+++ SenaThreads.Application/Dtos/Users/UserSearchHistoryDto.cs
@@ -4,4 +4,8 @@ public class UserSearchHistoryDto
     public int Id { get; set; }
     public string SearchedUserId { get; set; }
     public DateTime SearchedAt { get; set; }
+    public string Username { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string ProfilePictureS3Key { get; set; }
 }
--- SenaThreads.Application/Users/UserQueries/GetUserSearchHistory/GetUserSearchHistoryQueryHandler.cs
+++ SenaThreads.Application/Users/UserQueries/GetUserSearchHistory/GetUserSearchHistoryQueryHandler.cs
@@ -1,21 +1,28 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SenaThreads.Application.Abstractions.Messaging;
 using SenaThreads.Application.Dtos.Users;
+using SenaThreads.Application.ExternalServices;
 using SenaThreads.Application.IRepositories;
 using SenaThreads.Domain.Abstractions;
+using SenaThreads.Domain.Users;
 
 namespace SenaThreads.Application.Users.UserQueries.GetUserSearchHistory;
 public class GetUserSearchHistoryQueryHandler : IQueryHandler<GetUserSearchHistoryQuery, List<UserSearchHistoryDto>>
 {
     private readonly ISearchUserHistoryRepository _searchUserHistoryRepository;
     private readonly IMapper _mapper;
-    
+    private readonly IAwsS3Service _awsS3Service;
+    private readonly UserManager<User> _userManager;
 
-    public GetUserSearchHistoryQueryHandler(ISearchUserHistoryRepository searchUserHistoryRepository, IMapper mapper)
+
+    public GetUserSearchHistoryQueryHandler(ISearchUserHistoryRepository searchUserHistoryRepository, IMapper mapper, IAwsS3Service awsS3Service, UserManager<User> userManager)
     {
         _searchUserHistoryRepository = searchUserHistoryRepository;
         _mapper = mapper;
+        _awsS3Service = awsS3Service;
+        _userManager = userManager;
     }
 
     public async Task<Result<List<UserSearchHistoryDto>>> Handle(GetUserSearchHistoryQuery request, CancellationToken cancellationToken)
@@ -24,15 +31,37 @@ public class GetUserSearchHistoryQueryHandler : IQueryHandler<GetUserSearchHisto
             .Where(h => h.UserId == request.UserId)
             .OrderByDescending(h => h.SearchedAt)
             .Take(request.Limit)
-            .Select(h => new UserSearchHistoryDto
-            {
-                Id = h.Id,
-                SearchedUserId = h.SearchedUserId,
-                SearchedAt = h.SearchedAt
-            })
+            .Select(h => new { h.Id, h.SearchedUserId, h.SearchedAt })
             .ToListAsync(cancellationToken);
 
-        return Result.Success(searchHistory);
+        var userSearchHistoryDtos = new List<UserSearchHistoryDto>();
+
+        foreach (var history in searchHistory)
+        {
+            var searchedUser = await _userManager.FindByIdAsync(history.SearchedUserId);
+            if (searchedUser != null)
+            {
+                var dto = new UserSearchHistoryDto
+                {
+                    Id = history.Id,
+                    SearchedUserId = history.SearchedUserId,
+                    SearchedAt = history.SearchedAt,
+                    Username = searchedUser.UserName,
+                    FirstName = searchedUser.FirstName,
+                    LastName = searchedUser.LastName,
+                    ProfilePictureS3Key = searchedUser.ProfilePictureS3Key
+                };
+
+                if (!string.IsNullOrEmpty(dto.ProfilePictureS3Key))
+                {
+                    dto.ProfilePictureS3Key = _awsS3Service.GeneratePresignedUrl(dto.ProfilePictureS3Key);
+                }
+
+                userSearchHistoryDtos.Add(dto);
+            }
+        }
+
+        return Result.Success(userSearchHistoryDtos);
     }
 }
 
