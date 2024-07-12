344d4b951f73b518215cec631463aeb6b3c70fee
manuelsolart3 <118754640+manuelsolart3@users.noreply.github.com>
2024-07-12 12:53:43 -0500

    [Add]BlockFilter in GetUserProfile

--- SenaThreads.Application/Users/UserQueries/GetUserProfile/GetUserProfileQueryHandler.cs
+++ SenaThreads.Application/Users/UserQueries/GetUserProfile/GetUserProfileQueryHandler.cs
@@ -4,6 +4,7 @@ using SenaThreads.Application.Abstractions.Messaging;
 using SenaThreads.Application.Dtos.Users;
 using SenaThreads.Application.ExternalServices;
 using SenaThreads.Application.IRepositories;
+using SenaThreads.Application.IServices;
 using SenaThreads.Domain.Abstractions;
 using SenaThreads.Domain.Users;
 
@@ -13,15 +14,21 @@ public class GetUserProfileQueryHandler : IQueryHandler<GetUserProfileQuery, Use
     private readonly UserManager<User> _userManager;
     private readonly IMapper _mapper;
     private readonly IAwsS3Service _awsS3Service;
+    private readonly IBlockFilterService _blockFilterService;
+    private readonly ICurrentUserService _currentUserService;
 
     public GetUserProfileQueryHandler(
         IMapper mapper,
         UserManager<User> userManager,
-        IAwsS3Service awsS3Service)
+        IAwsS3Service awsS3Service,
+        IBlockFilterService blockFilterService,
+        ICurrentUserService currentUserService)
     {
         _mapper = mapper;
         _userManager = userManager;
         _awsS3Service = awsS3Service;
+        _blockFilterService = blockFilterService;
+        _currentUserService = currentUserService;
     }
 
     public async Task<Result<UserProfileDto>> Handle(GetUserProfileQuery request, CancellationToken cancellationToken)
@@ -30,19 +37,27 @@ public class GetUserProfileQueryHandler : IQueryHandler<GetUserProfileQuery, Use
 
         if (user is null)
         {
-        return Result.Failure<UserProfileDto>(UserError.UserNotFound);
+            return Result.Failure<UserProfileDto>(UserError.UserNotFound);
+        }
+
+        var currentUserId = _currentUserService.UserId;
+        var shouldFilter = await _blockFilterService.FilterBlockedContent(new[] { user }, currentUserId, u => u.Id);
+
+        if (!shouldFilter.Any())
+        {
+            return Result.Failure<UserProfileDto>(UserError.UserBlocked); 
         }
 
         // Mapear la entidad de usuario a un DTO de perfil
-         UserProfileDto userProfileDto = _mapper.Map<UserProfileDto>(user);
+        UserProfileDto userProfileDto = _mapper.Map<UserProfileDto>(user);
 
-        if(!string.IsNullOrEmpty(user.ProfilePictureS3Key))
+        if (!string.IsNullOrEmpty(user.ProfilePictureS3Key))
         {
             userProfileDto.ProfilePictureS3Key = _awsS3Service.GeneratePresignedUrl(user.ProfilePictureS3Key);
         }
 
         // Retornar el DTO de perfil de usuario
         return Result.Success(userProfileDto);
-
     }
 }
+
