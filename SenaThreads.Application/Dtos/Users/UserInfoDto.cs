5484c48840356a57313f9fb54cdb663d214a6b55
manuelsolart3 <118754640+manuelsolart3@users.noreply.github.com>
2024-07-12 18:11:00 -0500

    [Add] profilePicture in GetUserInfo

--- SenaThreads.Application/Dtos/Users/UserInfoDto.cs
+++ SenaThreads.Application/Dtos/Users/UserInfoDto.cs
@@ -3,4 +3,5 @@ public class UserInfoDto
 {
     public string UserId { get; set; }
     public string UserName { get; set; }
+    public string ProfilePictureS3Key { get; set; }
 }
--- SenaThreads.Application/Users/UserQueries/GetUserInfo/GetUserInfoQueryHandler.cs
+++ SenaThreads.Application/Users/UserQueries/GetUserInfo/GetUserInfoQueryHandler.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using SenaThreads.Application.Abstractions.Messaging;
 using SenaThreads.Application.Dtos.Users;
+using SenaThreads.Application.ExternalServices;
 using SenaThreads.Application.IServices;
 using SenaThreads.Domain.Abstractions;
 using SenaThreads.Domain.Users;
@@ -13,11 +14,13 @@ public class GetUserInfoQueryHandler : IQueryHandler<GetUserInfoQuery, UserInfoD
     private readonly UserManager<User> _userManager;
     private readonly IUserContextService _userContextService;
     private readonly IMapper _mapper;
-    public GetUserInfoQueryHandler(UserManager<User> userManager, IUserContextService userContextService, IMapper mapper)
+    private readonly IAwsS3Service _awsS3Service;
+    public GetUserInfoQueryHandler(UserManager<User> userManager, IUserContextService userContextService, IMapper mapper, IAwsS3Service awsS3Service)
     {
         _userManager = userManager;
         _userContextService = userContextService;
         _mapper = mapper;
+        _awsS3Service = awsS3Service;
     }
 
     public async Task<Result<UserInfoDto>> Handle(GetUserInfoQuery request, CancellationToken cancellationToken)
@@ -40,6 +43,12 @@ public class GetUserInfoQueryHandler : IQueryHandler<GetUserInfoQuery, UserInfoD
 
         // Mapear el usuario a UserInfoDto
         UserInfoDto userInfoDto = _mapper.Map<UserInfoDto>(user);
+
+        // Agregar la lógica para la imagen de perfil
+        if (!string.IsNullOrEmpty(user.ProfilePictureS3Key))
+        {
+            userInfoDto.ProfilePictureS3Key = _awsS3Service.GeneratePresignedUrl(user.ProfilePictureS3Key);
+        }
 
         return Result.Success(userInfoDto);
     }
